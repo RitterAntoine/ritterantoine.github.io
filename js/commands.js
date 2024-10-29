c8707b69e1e73e632603fd8ad3331a74ec063924
RitterAntoine <116518867+RitterAntoine@users.noreply.github.com>
2024-10-29 18:01:11 +0100

    feat: new design

---
+++ js/commands.js
@@ -0,0 +1,57 @@
+var password = "lumine";
+var linkedin = "https://www.linkedin.com/in/antoine-ritter-cesi/";
+var github = "https://github.com/RitterAntoine";
+var email = 'mailto:antoine.ritter@viacesi.fr';
+
+whoami = [
+  "<br>",
+  "Hi! I'm Antoine!",
+  "I am a second-year student in the computer engineering program (BAC+4).", 
+  "Passionate about technology, I am motivated to apply my programming and problem-solving skills in practical scenarios.",
+  "My goal is to explore various fields in computer science during my academic and professional journey.",
+  "My interest in research stems from my previous experiences working in both laboratories and a research centers.",
+  "These experiences have fueled my passion for delving deeper into the realms of computer science and discovering",
+  "innovative solutions to complex problems. Therefore, I am eager to further pursue a career in research,",
+  "where I can contribute to advancements in technology and make meaningful contributions to the field.",
+  "like this terminal website.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/ritterantoine' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/ritterantoine' + "</a>",
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whoami</span>         Who am I?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  "                                        ",
+  "      __                    _            ",
+  "     / /   __  ______ ___  (_)___  ___   ",
+  "    / /   / / / / __ `__ \\/ / __ \\/ _ \\  ",
+  "   / /___/ /_/ / / / / / / / / / /  __/  ",
+  "  /_____/\\__,_/_/ /_/ /_/_/_/ /_/\\___/   ",
+  "                                        ",
+  "                                        ",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
